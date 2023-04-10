f0642a303522ee1819da25305ccaf498547a0791
Mr Atass <wleow.ershov@gmail.com>
2023-04-10 18:38:51 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/MainForm.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.importButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxLogin = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxLLogin = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(152, 12);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.Size = new System.Drawing.Size(760, 427);
+            this.dataGrid.TabIndex = 1;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(12, 12);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(122, 27);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // importButton
+            // 
+            this.importButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.importButton.Location = new System.Drawing.Point(12, 111);
+            this.importButton.Name = "importButton";
+            this.importButton.Size = new System.Drawing.Size(122, 27);
+            this.importButton.TabIndex = 3;
+            this.importButton.Text = "Delete";
+            this.importButton.UseVisualStyleBackColor = false;
+            this.importButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(12, 45);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(122, 27);
+            this.addButton.TabIndex = 4;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 27);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Edit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(15, 270);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(131, 20);
+            this.textBoxName.TabIndex = 8;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(15, 319);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(131, 20);
+            this.textBoxPassword.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Имя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 303);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Пароль";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 403);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Почта";
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.AutoSize = true;
+            this.textBoxLogin.Location = new System.Drawing.Point(15, 354);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(38, 13);
+            this.textBoxLogin.TabIndex = 16;
+            this.textBoxLogin.Text = "Логин";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(15, 419);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(131, 20);
+            this.textBoxEmail.TabIndex = 15;
+            // 
+            // textBoxLLogin
+            // 
+            this.textBoxLLogin.Location = new System.Drawing.Point(15, 370);
+            this.textBoxLLogin.Name = "textBoxLLogin";
+            this.textBoxLLogin.Size = new System.Drawing.Size(131, 20);
+            this.textBoxLLogin.TabIndex = 14;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(18, 445);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(897, 415);
+            this.webBrowser1.TabIndex = 18;
+            this.webBrowser1.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.webBrowser1_Navigating);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 883);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxLogin);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxLLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.importButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.dataGrid);
+            this.Name = "MainForm";
+            this.Text = "Рабочая Форма";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button importButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label textBoxLogin;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxLLogin;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ WindowsFormsApp1/MainForm.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class MainForm : Form
+    {
+        MySqlConnection connection = new MySqlConnection("Server=localhost;Port=3306;Database=mydb;Uid=tarkov;Pwd=[(Kqx-nritx8jbHy;");
+        public MainForm()
+        {
+            InitializeComponent();
+            try
+            {
+                connection.Open();
+                MySqlCommand command = new MySqlCommand("SELECT * FROM users", connection);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dataGrid.DataSource = table;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("www.google.com");
+        }
+        private void webBrowser1_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            this.Text = e.Url.ToString() + "Is loading ..........";
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+
+                string sql = "INSERT INTO users (name, password, login, email) VALUES (@name, @password, @login, @email)";
+                MySqlCommand command = new MySqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@name", textBoxName.Text);
+                command.Parameters.AddWithValue("@password", textBoxPassword.Text);
+                command.Parameters.AddWithValue("@login", textBoxLogin.Text);
+                command.Parameters.AddWithValue("@email", textBoxEmail.Text);
+
+                int rowsAffected = command.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Пользователь добавлен успешно.");
+                }
+                else
+                {
+                    MessageBox.Show("Пользователь не добавлен.");
+                }
+
+                MySqlCommand selectCommand = new MySqlCommand("SELECT * FROM users", connection);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(selectCommand);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dataGrid.DataSource = table;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при добавлении пользователя: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+
+                string sql = "UPDATE users SET name=@name, password=@password, login=@login, email=@email WHERE id=@id";
+                MySqlCommand command = new MySqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@name", textBoxName.Text);
+                command.Parameters.AddWithValue("@password", textBoxPassword.Text);
+                command.Parameters.AddWithValue("@login", textBoxLogin.Text);
+                command.Parameters.AddWithValue("@email", textBoxEmail.Text);
+                command.Parameters.AddWithValue("@id", dataGrid.CurrentRow.Cells["id"].Value);
+
+                int rowsAffected = command.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Пользователь обновлен успешно.");
+                }
+                else
+                {
+                    MessageBox.Show("Пользователь не обновлен.");
+                }
+
+                MySqlCommand selectCommand = new MySqlCommand("SELECT * FROM users", connection);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(selectCommand);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dataGrid.DataSource = table;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при обновлении пользователя: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+
+                string sql = "DELETE FROM users WHERE id=@id";
+                MySqlCommand command = new MySqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@id", dataGrid.CurrentRow.Cells["id"].Value);
+
+                int rowsAffected = command.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Пользователь удален успешно.");
+                }
+                else
+                {
+                    MessageBox.Show("Пользователь не удален.");
+                }
+
+                MySqlCommand selectCommand = new MySqlCommand("SELECT * FROM users", connection);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(selectCommand);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dataGrid.DataSource = table;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при удалении пользователя: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
